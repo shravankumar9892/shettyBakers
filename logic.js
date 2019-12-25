d96244f9e84568c2b37690bf41a310cdd7f25d41
shravankumar9892 <shravankumarshetty9892@gmail.com>
2019-12-25 14:33:20 +0530

    emailjs added

--- logic.js
+++ logic.js
@@ -1,11 +1,29 @@
-function contactus() {
-    var elem = document.getElementById("contact-us");
-    elem.value = "Call 543216789!";
-    elem.style.transform = "scale(2.5)";
+var cakeList = []; // Order list with tags
+var orderList = []; // Order list final
+var nameParticipant = ""; // Participant name
+
+
+// Sending email on place order
+function sendEmail() {
+    var template_params = {
+        "reply_to": "reply_to_value",
+        "from_name": String(nameParticipant),
+        "message_html": "<p>"+String(orderList)+"</p>"
+     }
+     
+     var service_id = "default_service";
+     var template_id = "template_QzIle3i7";
+     emailjs.send(service_id, template_id, template_params);
+     alert('Order has been placed successfully. All the best!');
+}
+// To store name from modal
+function storedata() {
+    nameParticipant = document.getElementById("participant").value;
+    console.log(nameParticipant);
 }
 
-var cakeList = [];
 
+// Function to add pastries into the list on blackboard
 function addList(clickedID) {
     var elem = document.getElementById(String(clickedID));
     cakeList.push(elem);
@@ -19,6 +37,7 @@ function addList(clickedID) {
     x.setAttribute("id", String(clickedID));
     // Adding cake name
     var x_ = document.createTextNode(elem.children[0].textContent);
+    orderList.push(elem.children[0].textContent);
     // Adding cake name to li
     x.appendChild(x_);
     // Addign li to ul
